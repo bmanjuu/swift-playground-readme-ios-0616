561604124332420125956bc19850418455f3d8fc
bmanjuu <bettyofung@gmail.com>
2016-07-06 09:22:01 -0400

    completed

--- Playgrounds.playground/Contents.swift
+++ Playgrounds.playground/Contents.swift
@@ -1,2 +1,3 @@
 let str = "Hello, world"
-str = "Goodbye, world"
+// str = "Goodbye, world"
+// error pops up saying that you can't change it b/c variable is a constant. suggests changing "let" to "var"
